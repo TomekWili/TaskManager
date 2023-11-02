4f95508cef29d28f41c9cabb5b7df0129fb3d4d5
TomekWili <twili@wp.pl>
2023-11-02 01:23:24 +0100

    FirstCheck

---
+++ Warsztat/TaskManager/TaskManager/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class Task
+    {
+    static private int Id;
+    public string Description;
+    public string CreationDate;
+    public string DueDate;
+    public string StartDate;
+    public string DoneDate;
+
+
+    static void SetId()
+    {
+        Id++;
+    }
+}
+}
